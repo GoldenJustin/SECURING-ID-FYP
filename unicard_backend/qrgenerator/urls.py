0b7beda9db2c9ff23b7481997f23379ff596c78a
justin <justinemsengi@gmail.com>
2023-07-06 10:14:59 +0300

    futher improvements

--- unicard_backend/qrgenerator/urls.py
+++ unicard_backend/qrgenerator/urls.py
@@ -1,9 +1,2 @@
 from django.urls import path
-from .views import generate_qr, decrypt_text
 
-app_name = 'qrgenerator'
-
-urlpatterns = [
-    path('generate/<str:student_code>/', generate_qr, name='generate_qr'),
-    path('decrypt/', decrypt_text, name='decrypt_text'),
-]
--- unicard_backend/unicard_backend/urls.py
+++ unicard_backend/unicard_backend/urls.py
@@ -8,5 +8,4 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accountsAPI.urls')),
-    path('qrcode/', include('qrgenerator.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
