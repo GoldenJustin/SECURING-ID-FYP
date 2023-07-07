d4bb236911f7c750aa8d79f2a7dee57704b6ac7a
justin <justinemsengi@gmail.com>
2023-07-07 12:35:50 +0300

    further modifications

--- unicard_backend/accountsAPI/urls.py
+++ unicard_backend/accountsAPI/urls.py
@@ -15,4 +15,4 @@ urlpatterns = [
     path('user/login/', UserLoginAPIView.as_view(), name="login_user"),
     path('user/logout/', UserLogoutAPIView.as_view(), name="logout_user"),
     path('', index, name="index"),
-    path('cardDetails/<str:student_code>/', id_card_data, name="id_card_data"),]
+    path('cardDetails/<str:student_code>/', id_card_data, name="card_data"),]
